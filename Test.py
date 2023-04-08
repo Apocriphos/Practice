7709c327c22b7c010fbf0ea57ab8fc0eab0b3ce9
Emirhan Baran <emirhanbaran26@gmail.com>
2023-04-08 19:44:38 +0300

    test commited

--- Test.py
+++ Test.py
@@ -1,109 +1,135 @@
-P= input("Çemberde bulunacak oyuncu sayısını giriniz: ") 
+player= input("Çemberde bulunacak oyuncu sayısını giriniz: ") 
+
+def hata():
+    print("Hatalı değer girdiniz lütfen tekrar deneyiniz.")
+
+
+
 
 try: 
-    P = int(P)
-    while P <= 1:
-        print("Hatalı değer girdiniz lütfen tekrar deneyiniz.")
-        P= input("Çemberde bulunacak oyuncu sayısını giriniz: ")
-        if P > 1:
+    player = int(player)
+    while player <= 1:
+        hata()
+        player= input("Çemberde bulunacak oyuncu sayısını giriniz: ")
+        if player > 1:
             break
         else:
             print("eksik değer girdiniz lütfen tekrar deneyiniz.")
 except:
-    print("Hatalı değer girdiniz lütfen tekrar deneyiniz.")
-    while type(P) is not int:
-        P= (input("Çemberde bulunacak oyuncu sayısını giriniz: "))
+    hata()
+    while type(player) is not int:
+        player= (input("Çemberde bulunacak oyuncu sayısını giriniz: "))
         try:
-            P = int(P)
-            if type(P) is int:
-                while P <= 1:
-                    print("Hatalı değer girdiniz lütfen tekrar deneyiniz.")
-                    P= input("Çemberde bulunacak oyuncu sayısını giriniz: ")
+            player = int(player)
+            if type(player) is int:
+                while player <= 1:
+                    hata()
+                    player= input("Çemberde bulunacak oyuncu sayısını giriniz: ")
                     try:
-                        P = int(P)
-                        if P > 1:
+                        player = int(player)
+                        if player > 1:
                             break
                         else:
                             print("eksik değer girdiniz lütfen tekrar deneyiniz.")
                     except:
-                        print("Hatalı değer girdiniz lütfen tekrar deneyiniz.")
+                        hata()
             else:
-                print("Hatalı değer girdiniz lütfen tekrar deneyiniz.")
+                hata()
         except:
-            print("Hatalı değer girdiniz lütfen tekrar deneyiniz.")
+            hata()
+player = int(player)
 
-p = P
-c = 1
+cycle = 1
 
+player_alive = player
+def minus(a):
+    a = a - 1
+def kill(a):
+    oyuncular[a] = ">-Ölü-<"
+def kill_next():
+    for t in range(1, player):
+        if i+t < player:
+            if oyuncular[i+t] == "Canlı":
+                kill(i+t)
+                player_alive = player_alive - 1
+            elif oyuncular[i+t] == ">-Ölü-<":
+                continue
+        else:
+            if oyuncular[0] == "Canlı":
+                kill(0)
+                player_alive = player_alive - 1
+                if player_alive == 1:
+                    break
+            elif oyuncular[0] == ">-Ölü-<":
+                for h in range(player):
+                    if oyuncular[h] == "Canlı":
+                        kill(h)
+                        player_alive = player_alive - 1
+                        break
 oyuncular = []
-for i in range(P):
+for i in range(player):
     oyuncular.append("Canlı")
-
-while p > 1:
-    for i in range(P):
-        if i == P-1:
-            c = c + 1
-            if oyuncular[i] == "Canlı":
+while player_alive > 1:
+    for i in range(player):
+        if i == player-1:
+            cycle = cycle + 1
+            for t in range(1, player):
+                if i+t < player:
+                    if oyuncular[i+t] == "Canlı":
+                        kill(i+t)
+                        player_alive = player_alive - 1
+                    elif oyuncular[i+t] == ">-Ölü-<":
+                        continue
+                else:
                     if oyuncular[0] == "Canlı":
-                        oyuncular[0] = ">Ölü<"
-                        print(oyuncular, i+1, ". oyuncunun sırası")
-                        p = p - 1
-                        print(p)
-                        if p == 1:
+                        kill(0)
+                        player_alive = player_alive - 1
+                        if player_alive == 1:
                             break
-                    elif oyuncular[0] == ">Ölü<":
-                        for t in range(1, P):
-                            if oyuncular[t] == ">Ölü<":
-                                continue
-                            elif oyuncular[t] == "Canlı":
-                                oyuncular[t] = ">Ölü<"
-                                print(oyuncular, i+1, ". oyuncunun sırası.")
-                                p = p - 1
-                                print(p)
-                                if p == 1:
-                                    break
-                                continue
-            elif oyuncular[i] == ">Ölü<":
-                break
+                    elif oyuncular[0] == ">-Ölü-<":
+                        for h in range(player):
+                            if oyuncular[h] == "Canlı":
+                                kill(h)
+                                player_alive = player_alive - 1
+                                break
         else:
             if oyuncular[i] == "Canlı":
-                for t in range(1, P):
-                    if i+t < P:
+                for t in range(1, player):
+                    if i+t < player:
                         if oyuncular[i+t] == ">Ölü<":
                             continue
                         elif oyuncular[i+t] == "Canlı":
                             oyuncular[i+t] = ">Ölü<"
                             print(oyuncular, i+1, ". oyuncunun sırası.")
-                            p = p - 1
-                            print(p)
-                            if p == 1:
+                            player_alive = player_alive - 1
+                            print(player_alive)
+                            if player_alive == 1:
                                 break
                             break
                     else:
-                        for h in range(P):
+                        for h in range(player):
                             if oyuncular[h] == ">Ölü<":
                                 continue
                             elif oyuncular[h] == "Canlı":
-                                if p == 1:
+                                if player_alive == 1:
                                     break
                                 else:
                                     oyuncular[h] = ">Ölü<"
                                     print(oyuncular, i+1, ". oyuncunun sırası.")
-                                    p = p - 1
-                                    print(p)
-                                    if p == 1:
+                                    player_alive = player_alive - 1
+                                    print(player_alive)
+                                    if player_alive == 1:
                                         break
                                     break
             elif oyuncular[i] == ">Ölü<":
-                if p == 1:
+                if player_alive == 1:
                     break
                 else:
                     print(oyuncular, i+1, ". oyuncu >ölü< olduğundan sırası geçildi.")
                     continue
-
-for b in range(P):
+for b in range(player):
     if oyuncular[b] == "Canlı":
-        print("Sona kalan oyuncu", b+1, ". Oyuncu olarak", c, "turda belirlenmiştir.")
+        print("Sona kalan oyuncu", b+1, ". Oyuncu olarak", cycle, "turda belirlenmiştir.")
         print("   --OYUN BİTTİ--   ")
     else:
         continue
